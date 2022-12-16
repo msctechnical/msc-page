2a284fdd0c116a1e1cf41131ef9c8a1597a60ae8
msctechnical <120730331+msctechnical@users.noreply.github.com>
2022-12-16 20:49:12 +0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <div className="navbar">
         <h1><a href="/">MSC TEACHNICAL</a></h1>
         <ul>
-          <li><Link to="msc-page/" href="#">Home</Link></li>
+          <li><Link to="/" href="#">Home</Link></li>
           <li><a href="#skills">Services</a></li>
           <li><Link to="/about" href="#">About Us</Link></li>
           <li><a href="#contact-us">Contact Us</a></li>
